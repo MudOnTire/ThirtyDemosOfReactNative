3f049754673a1e4f7554c89a17f7c03d1baa45ff
zhangjianyang <895157882@qq.com>
2017-11-21 17:04:32 +0800

    增加文件

---
+++ App.js
@@ -0,0 +1,72 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import Demo1 from './js/demo1';
+
+class MainView extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      Demos: [
+        {
+          key: 0,
+          title: 'A stopwatch',
+          component: Demo1,
+          isFA: false,
+          icon: 'ios-stopwatch',
+          size: 48,
+          color: '#ff856c',
+          hideNav: false
+        }
+      ]
+    }
+  }
+}
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit App.js
+        </Text>
+        <Text style={styles.instructions}>
+          {instructions}
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
