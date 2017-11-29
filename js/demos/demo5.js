530df1b6f4a9591fc64ce5ba7ce10c89684de69d
zhangjianyang <895157882@qq.com>
2017-11-29 11:41:44 +0800

    修改文件

--- js/demos/demo5.js
+++ js/demos/demo5.js
@@ -1,31 +1,133 @@
-import React, {Component} from 'react';
-import {Platform, Image, MapView, StatusBar, StyleSheet, Text, TouchableHighlight, View} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, Image, MapView, StatusBar, StyleSheet, Text, TouchableHighlight, View } from 'react-native';
 import PropTypes from 'prop-types'
 import Util from '../utils';
 import Icon from 'react-native-vector-icons/Ionicons';
 
-export class Map extends Component{
+console.log(MapView);
+
+const styles = StyleSheet.create({
+    container:{
+      alignItems: "center",
+      paddingTop: 60
+    },
+    map:{
+      width: Util.size.width,
+      height: Util.size.height-120
+    },
+    btn:{
+      backgroundColor:"#00a803",
+      width: Util.size.width-80,
+      height: 40,
+      borderWidth:Util.pixel,
+      borderColor: "#009302",
+      borderRadius: 4,
+      justifyContent:"center",
+      marginTop:10
+    },
+    btnText:{
+      textAlign:"center",
+      fontSize:18,
+      color:"#fff"
+    },
+  });
+
+class Map extends Component {
     static defaultProps = {
-        mapType:"standard",
-        showUserLocation:false,
-        followUserLocation:false,
+        mapType: "standard",
+        showUserLocation: false,
+        followUserLocation: false,
     };
 
-    static propTypes={
-        mapType:React.propTypes.oneOf(["standard", "satellite", "hybrid"]),
-        showUserLocation: React.PropTypes.bool.isRequired,
-        followUserLocation:React.PropTypes.bool.isRequired,
+    static propTypes = {
+        mapType: PropTypes.oneOf(["standard", "satellite", "hybrid"]),
+        showUserLocation: PropTypes.bool.isRequired,
+        followUserLocation: PropTypes.bool.isRequired,
     }
 
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
-            isFirstLoad:true,
-            mapRegion:undefined,
-            annotations:[],
+        this.state = {
+            isFirstLoad: true,
+            mapRegion: undefined,
+            annotations: [],
         };
     }
 
+    _getAnnotations(region) {
+        return [{
+            longitude: region.longitude,
+            latitude: region.latitude,
+            title: 'You are here',
+        }];
+    }
 
+    _onRegionChangeComplete(region) {
+        if (this.state.isFirstLoad) {
+            this.state({
+                annotations: this._getAnnotations(region),
+                isFirstLoad: false,
+            });
+        }
+    }
 
+    render() {
+        return (
+            <View>
+                <MapView
+                    style={this.props.mapStyle}
+                    mapType={this.props.mapType}
+                    showsUserLocation={this.props.showsUserLocation}
+                    followUserLocation={this.props.followUserLocation}
+                    onRegionChangeComplete={(region) => this._onRegionChangeComplete(region)}
+                    region={this.state.mapRegion}
+                    annotations={this.state.annotations}
+                />
+            </View>
+        )
+    }
+}
+
+export default class Demo5 extends Component {
+    constructor() {
+        super();
+        this.state = {
+            showGeo: false
+        };
+    }
+
+    componentDidMount(){
+        if(Platform.OS==='ios'){
+            StatusBar.setBarStyle(0);
+        }
+    }
+
+    _getLocation(){
+        this.setState({
+            showGeo:true
+        })
+    }
+
+    render(){
+        return (
+            <View style={styles.container}>
+                <Map 
+                    mapType='standard' 
+                    mapStyle={styles.map} 
+                    showsUserLocation={this.state.showGeo}
+                    followUserLocation={this.state.showGeo}
+                ></Map>
+                <TouchableHighlight 
+                    underlayColor="#00bd03"
+                    style={styles.btn}
+                    onPress={()=>this._getLocation()}
+                >
+                    <Text style={styles.btnText}>
+                        <Icon size={18} name='md-navigate'></Icon>
+                        Find my location
+                    </Text>
+                </TouchableHighlight>
+            </View>
+        )
+    }
 }
--- js/navigators/stackNavigator.js
+++ js/navigators/stackNavigator.js
@@ -4,10 +4,11 @@ import Home from '../screens/home';
 import Demo1 from '../demos/demo1';
 import Demo2 from '../demos/demo2';
 import Demo3 from '../demos/demo3';
-import {Demo3TopNav} from '../demos/demo3';
+import { Demo3TopNav } from '../demos/demo3';
+import Demo5 from '../demos/demo5';
 
 const navigator = StackNavigator({
-    Home: { 
+    Home: {
         screen: Home,
         navigationOptions: ({ navigation }) => ({
             title: "30 demos of React-Native for 30 days"
@@ -16,23 +17,27 @@ const navigator = StackNavigator({
     Demo1: {
         screen: Demo1,
         navigationOptions: ({ navigation }) => ({
-            header:null
+            header: null
         }),
     },
     Demo2: {
         screen: Demo2,
         navigationOptions: ({ navigation }) => ({
-            header:null
-        }), 
+            header: null
+        }),
     },
     Demo3: {
         screen: Demo3,
         navigationOptions: ({ navigation }) => ({
-            header:<Demo3TopNav></Demo3TopNav>
-        }), 
+            header: <Demo3TopNav></Demo3TopNav>
+        }),
     },
-},{
-
+    Demo5: {
+        screen: Demo5,
+        navigationOptions: ({ navigation }) => ({
+            header: null
+        })
+    }
 });
 
 export default navigator;
