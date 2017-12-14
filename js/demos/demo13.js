bc9db041b095a0f4df183489bda119aa8ee67a8f
zhangjianyang <895157882@qq.com>
2017-12-14 09:58:36 +0800

    修改demo13

--- js/demos/demo13.js
+++ js/demos/demo13.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import { StatusBar, View, StyleSheet, Image, TextInput, CameraRoll, TouchableHighlight, Text, Keyboard } from 'react-native';
+import { StatusBar, View, StyleSheet, Image, TextInput, CameraRoll, TouchableHighlight, Text, Keyboard, Button, Alert } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
 import PropTypes from 'prop-types';
 import Util from '../utils';
+
+const functionViewHeight = 50;
 
 const styles = StyleSheet.create({
     container: {
@@ -13,31 +15,55 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-between',
     },
-    textArea: {
+    closeBtn: {
+        width: 30,
+        height: 30,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+    , textArea: {
         padding: 15,
-        fontSize: 20,
+        fontSize: 18,
         textAlignVertical: 'top',
     },
     functionViewContainer: {
         position: 'absolute',
-        height:40,
-        width:Util.size.width,
-        left:0,
+        height: functionViewHeight,
+        width: Util.size.width,
+        left: 0,
     },
     functionContainer: {
-        height: 40,
-        justifyContent: 'center',
-    },
-    functionIconContainer: {
+        height: functionViewHeight,
+        justifyContent: 'space-between',
         flexDirection: 'row',
+        borderTopWidth: 1,
+        borderTopColor: '#ccc',
+        alignItems: 'center',
+        paddingRight: 8,
     },
-    functionIcon: {
-        flex: 3,
+    functionIcons: {
         flexDirection: 'row',
         justifyContent: 'space-around',
     },
-    functionBtn: {
-        flex: 1
+    functionIcon: {
+        width: 50,
+        textAlign: 'center',
+    },
+    functionRight: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    postBtn: {
+        backgroundColor: '#0090FF',
+        width: 60,
+        height: 34,
+        borderRadius: 6,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    remainCount: {
+        fontSize: 16,
+        marginRight: 10,
     }
 });
 
@@ -87,18 +113,19 @@ class FuntionView extends Component {
     render() {
         return (
             <View style={styles.functionContainer}>
-                <View style={styles.functionIconContainer}>
-                    <View style={styles.functionIcon}>
-                        <Icon name='ios-pin' size={23} color='#8899a5'></Icon>
-                        <Icon name='md-camera' size={23} color='#8899a5'></Icon>
-                        <Icon name='md-image' size={23} color='#8899a5'></Icon>
-                        <Icon name='md-pie' size={23} color='#8899a5'></Icon>
-                    </View>
-                    <View style={styles.functionBtn}>
-                        <Text style={styles.text}>
-                            {this.props.numsOfText}
-                        </Text>
-                    </View>
+                <View style={styles.functionIcons}>
+                    <Icon style={styles.functionIcon} name='ios-pin' size={23} color='#8899a5'></Icon>
+                    <Icon style={styles.functionIcon} name='md-camera' size={23} color='#8899a5'></Icon>
+                    <Icon style={styles.functionIcon} name='md-image' size={23} color='#8899a5'></Icon>
+                    <Icon style={styles.functionIcon} name='md-pie' size={23} color='#8899a5'></Icon>
+                </View>
+                <View style={styles.functionRight}>
+                    <Text style={styles.remainCount}>
+                        {this.props.numsOfText}
+                    </Text>
+                    <TouchableHighlight style={styles.postBtn}>
+                        <Text style={{ color: '#fff', fontSize: 14 }}>发推</Text>
+                    </TouchableHighlight>
                 </View>
             </View>
         )
@@ -136,26 +163,44 @@ export default class Demo13 extends Component {
         })
     }
 
+    _closePage = () => {
+        Alert.alert(
+            '确认推出发布吗？',
+            null,
+            [
+                { text: '确认', onPress: () => this.props.navigation.goBack() },
+                { text: '取消', onPress: () => console.log('cancel close'), style: 'cancel' }
+            ],
+            { cancelable: true }
+        )
+    }
+
     render() {
         return (
             <View style={styles.container}>
                 <View style={styles.iconContainer}>
                     <Icon name='logo-github' size={30} color='#000'></Icon>
-                    <Icon name='md-close' color='#2aa2ef' size={25}></Icon>
+                    <TouchableHighlight
+                        style={styles.closeBtn}
+                        underlayColor='rgba(0,0,0,0)'
+                        onPress={this._closePage}>
+                        <Icon name='md-close' color='#2aa2ef' size={25}></Icon>
+                    </TouchableHighlight>
                 </View>
                 <TextInput
                     ref='textArea'
-                    style={styles.textArea}
+                    style={[styles.textArea, { height: Util.size.height - this.state.keyboardTopY - 60 - functionViewHeight }]}
                     maxLength={140}
                     multiline={true}
                     placeholder="有什么新鲜事？"
                     selectionColor='#2aa2ef'
-                    placeholderTextColor='#ced8de'
+                    placeholderTextColor='#bdc7cd'
                     onChangeText={this._updateTextNum}
+                    underlineColorAndroid='transparent'
                 >
                 </TextInput>
                 <View style={[styles.functionViewContainer, { bottom: this.state.keyboardTopY }]}>
-                    <FuntionView />
+                    <FuntionView numsOfText={this.state.numsOfText} />
                 </View>
             </View>
         )
